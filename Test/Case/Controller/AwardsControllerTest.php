d70c465bd5e200445aa2d2217ca783b5247117d2
Dave Mainville <dave@superdave2u.com>
2014-03-14 22:11:54 -0400

    awards scaffold

---
+++ Test/Case/Controller/AwardsControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('AwardsController', 'Controller');
+
+/**
+ * AwardsController Test Case
+ *
+ */
+class AwardsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.award'
+	);
+
+}
---
+++ Test/Case/Model/AwardTest.php
@@ -0,0 +1,58 @@
+<?php
+App::uses('Award', 'Model');
+
+/**
+ * Award Test Case
+ *
+ */
+class AwardTest extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.award',
+		'app.game',
+		'app.activity',
+		'app.activities_game',
+		'app.run',
+		'app.round',
+		'app.division',
+		'app.team_pair',
+		'app.activities_run',
+		'app.level',
+		'app.event',
+		'app.tour',
+		'app.events_game',
+		'app.award_condition',
+		'app.dog',
+		'app.awards_dog',
+		'app.handler',
+		'app.awards_handler',
+		'app.awards_team_pair'
+	);
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->Award = ClassRegistry::init('Award');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Award);
+
+		parent::tearDown();
+	}
+
+}
