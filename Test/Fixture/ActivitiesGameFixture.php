83a81344a832cb35d98299239a68eba822938ff8
Dave Mainville <dave@superdave2u.com>
2014-03-14 22:05:40 -0400

    activities games scaffold

---
+++ Test/Fixture/ActivitiesGameFixture.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * ActivitiesGameFixture
+ *
+ */
+class ActivitiesGameFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'activity_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'index'),
+		'game_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'index'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'activity_id' => array('column' => 'activity_id', 'unique' => 0),
+			'game_id' => array('column' => 'game_id', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'activity_id' => 1,
+			'game_id' => 1
+		),
+	);
+
+}
