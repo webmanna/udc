5d84650649cc374c70d0b67157913730deeb2a50
Dave Mainville <dave@superdave2u.com>
2014-03-16 11:25:29 -0400

    added json view controllers to events

--- Controller/EventsController.php
+++ Controller/EventsController.php
@@ -3,14 +3,48 @@ App::uses('AppController', 'Controller');
 /**
  * Events Controller
  *
+ * @property Event $Event
+ * @property PaginatorComponent $Paginator
  */
 class EventsController extends AppController {
-
 /**
  * Scaffold
  *
  * @var mixed
  */
 	public $scaffold = 'admin';
+        
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
 
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Event->recursive = 0;
+		$this->set('events', $this->Event->find('all'));
+                $this->set('_serialize', array( 'events' ) );
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Event->exists($id)) {
+			throw new NotFoundException(__('Invalid event'));
+		}
+		$options = array('conditions' => array('Event.' . $this->Event->primaryKey => $id));
+		$this->set('event', $this->Event->find('first', $options));
+                $this->set('_serialize', array( 'event' ) );
+	}
 }
--- Test/Case/Controller/EventsControllerTest.php
+++ Test/Case/Controller/EventsControllerTest.php
@@ -16,7 +16,63 @@ class EventsControllerTest extends ControllerTestCase {
 		'app.event',
 		'app.tour',
 		'app.game',
+		'app.action',
+		'app.actions_game',
+		'app.run',
+		'app.round',
+		'app.division',
+		'app.team_pair',
+		'app.handler',
+		'app.team',
+		'app.award',
+		'app.award_condition',
+		'app.dog',
+		'app.awards_dog',
+		'app.awards_handler',
+		'app.awards_team_pair',
+		'app.actions_run',
+		'app.level',
 		'app.events_game'
 	);
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+	}
+
+/**
+ * testEdit method
+ *
+ * @return void
+ */
+	public function testEdit() {
+	}
+
+/**
+ * testDelete method
+ *
+ * @return void
+ */
+	public function testDelete() {
+	}
 
 }
