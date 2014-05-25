66ea041e53abdc62871aba977026ab5dba8595ce
Dave Mainville <dave@superdave2u.com>
2014-05-25 16:22:18 -0400

    core include fix

--- Console/cake.php
+++ Console/cake.php
@@ -24,7 +24,7 @@ if (function_exists('ini_set')) {
 
 	// the following line differs from its sibling
 	// /app/Console/cake.php
-	ini_set('include_path', $root . PATH_SEPARATOR . 'C:' . $ds . 'xampp' . $ds . 'htdocs' . $ds . '!cake' . $ds . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
+	ini_set('include_path', $ROOT . PATH_SEPARATOR . $APP_DIR . $ds . 'Core' . $ds . 'lib' . PATH_SEPARATOR . ini_get('include_path'));
 }
 
 if (!include $dispatcher) {
