05ab8ef8d510af295d966590435ed208a9f05032
Dave Mainville <dave@superdave2u.com>
2014-05-25 16:26:51 -0400

    fixed paths

--- webroot/index.php
+++ webroot/index.php
@@ -52,7 +52,7 @@ if (!defined('APP_DIR')) {
  * The following line differs from its sibling
  * /app/webroot/index.php
  */
-define('CAKE_CORE_INCLUDE_PATH', 'C:' . DS . 'xampp' . DS . 'htdocs' . DS . '!cake' . DS . 'lib');
+define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . APP_DIR . DS . 'Core' . DS . 'lib');
 
 /**
  * Editing below this line should NOT be necessary.
